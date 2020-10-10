d6a1df2689e2667e65958b5a55b16ba8ab97c593
Champ2k <thehitmanza1@gmail.com>
2020-10-10 18:56:31 +0700

    validation user and collection session

--- routes/auth.js
+++ routes/auth.js
@@ -5,23 +5,63 @@ const router = express.Router()
 
 const User = require('../models/User')
 
-router.post('/register', (req, res) => {
-  const user = new User(req.body)
+router.post('/register', async (req, res) => {
+    const { username, password, name } = req.body
+    
+    if (!name || !username || !password) {
+      return res.render('register', { message: 'Please try again' })
+    }
+
+    const passwordHash = bcrypt.hashSync(password, 10)
+    const user = new User({
+    name,
+    username,
+    password: passwordHash
+  })
   await user.save()
   res.render('index', { user })
 })
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   const { username, password } = req.body
+  // simple validation
+  if (!username || !password) {
+    return res.render('register', { message: 'Please try again' })
+  }
   const user = await User.findOne({
-    username,
-    password
+    username
   })
   if (user) {
-    return res.render('index', { user })
+    const isCorrect = bcrypt.compareSync(password, user.password)
+    if (isCorrect) {
+      return res.render('index', { user })
+    } else {
+      return res.render('login', { message: 'Username or Password incorrect' })
+    }
   } else {
-    return res.render('login', { message: 'Email or Password incorrect' })
+    return res.render('login', { message: 'Username does not exist.' })
   }
 })
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  if (!username || !password) {
+      return res.render('register', { message: 'Please try again' })
+    }
+    const user = await User.findOne({
+        username
+    })
+    if (user) {
+        const isCorrect = bcrypt.compareSync(password, user.password)
+        if (isCorrect) {
+            req.user = user;
+            return res.render('index', { user })
+        }else {
+            return res.render('login', { message: 'Username or Password incorrect' })
+        }
+    }else {
+        return res.render('login', { message: 'Username does not exist.' })
+  }
+})
+
 
 module.exports = router
